c7752b624fe6f5fcfacf99ecd3062cc0bfdadd6e
U-NIFLHEIM\Lax <LaxWasHere@PoweredByHate.net>
2016-05-14 06:13:53 -0700

    .setRekt(true);

---
+++ src/main/java/net/poweredbyhate/eggrenade/Eggrenade.java
@@ -0,0 +1,88 @@
+package net.poweredbyhate.eggrenade;
+
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.economy.EconomyResponse;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Egg;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.logging.Level;
+
+public class Eggrenade extends JavaPlugin implements Listener {
+
+    /*
+    This plugin will be done in one class to demonstrate how stupid it is to be premium.
+     */
+
+    public static Economy econ = null;
+
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+        saveDefaultConfig();
+        checkVault();
+    }
+
+    @EventHandler
+    public void onHit(ProjectileHitEvent ev) {
+        if (ev.getEntity() instanceof Egg && ev.getEntity().getShooter() instanceof Player) {
+            Player p = (Player) ev.getEntity().getShooter();
+            if (p.hasPermission("eggrenade.active")) goBoom(p, ev.getEntity());
+        }
+    }
+
+    public void goBoom(Player p, Entity ent) {
+        if (econ != null) {
+            if (econ.getBalance(p) < getConfig().getInt("Cost")) {
+                return;
+            }
+            EconomyResponse e = econ.withdrawPlayer(p, getConfig().getInt("Cost"));
+            if (e.transactionSuccess()) {
+                doRealBoom(ent);
+                return;
+            }
+            return;
+        }
+        doRealBoom(ent);
+    }
+
+    public void doRealBoom(final Entity ent) {
+        final Location l = ent.getLocation();
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                ent.getWorld().createExplosion(l.getX(),l.getY(),l.getZ(), getConfig().getInt("Power"), getConfig().getBoolean("Destroy"), getConfig().getBoolean("Destroy"));
+            }
+        }.runTaskLater(this, getConfig().getInt("Delay") * 20);
+    }
+
+    public void checkVault() {
+        if (getConfig().getInt("Cost") != 0) {
+            if (!setupEconomy()) {
+                getLogger().log(Level.WARNING, "In order to use the economy support, you must have vault.");
+                getLogger().log(Level.WARNING, "Please set the cost to 0 in the config.yml to disable economy support");
+                getServer().getPluginManager().disablePlugin(this);
+            }
+        }
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+}
